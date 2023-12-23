269e8d71d1670bb5d56c0274028b0ff7721ca6f7
Anuj kattel <99376916+anujkattel@users.noreply.github.com>
2023-12-23 08:05:23 +0545

    Add files via upload

--- src/components/rightsidebar/Rightsidebar.jsx
+++ src/components/rightsidebar/Rightsidebar.jsx
@@ -1,13 +1,82 @@
-import React from 'react'
+import React from 'react';
+import img1 from "../../assets/Img/img1.jpg";
+import { Link } from 'react-router-dom';
+
+const data = [
+  { username: 'anup_kattel', Images: require("../../assets/Img/img1.jpg"), suggestedText: 'Suggested for you' },
+  { username: 'anup_', Images: require("../../assets/Img/img2.jpg"), suggestedText: 'Suggested for you' },
+  { username: 'anuj_', Images: require("../../assets/Img/img3.jpg"), suggestedText: 'Suggested for you' },
+  { username: 'ayush_', Images: require("../../assets/Img/img4.jpg"), suggestedText: 'Suggested for you' },
+];
+
+const Profile = ({ username, image, suggestedText }) => (
+  <div className="suggestions-profile-user flex">
+    <div className="small-flex">
+      <div className="logo-right">
+        <img src={image} alt="" />
+      </div>
+      <div className="username-right">
+        <span className="username-right-text">{username}</span>
+        <span>{suggestedText}</span>
+      </div>
+    </div>
+    <div className="follow">follow</div>
+  </div>
+);
 
 function Rightsidebar() {
   return (
     <>
-    <div className="right-sidebar">
-      Rightsidebar
-    </div>
+      <div className="right-sidebar">
+        <div className="sidebar-container">
+          <div className="profile-right-sidebar">
+            <div className="logo-right">
+              <img src={img1} alt="" />
+            </div>
+            <div className="username-right">
+              <span className="username-right-text">anujkattel</span>
+              <span className="name-text">Anuj kattel</span>
+            </div>
+          </div>
+          <div className="suggestions">
+            <div className="suggestion-text">
+              <h3>Suggested for you</h3>
+              <Link to="/all-suggestions">See All</Link>
+            </div>
+            <div>
+              {data.map((item, index) => (
+                <div className="suggestions-profile-user flex" key={index}>
+                  <div className="small-flex">
+                    <div className="logo-right">
+                      <img src={item.Images} alt="" />
+                    </div>
+                    <div className="username-right">
+                      <span className="username-right-text">{item.username}</span>
+                      <span>{item.suggestedText}</span>
+                    </div>
+                  </div>
+                  <div className="follow">follow</div>
+                </div>
+              ))}
+            </div>
+          </div>
+          <div className="link-area">
+            <Link to="/">About</Link>
+            <Link to="/">Help</Link>
+            <Link to="/">press</Link>
+            <Link to="/">API</Link>
+            <Link to="/">jobs</Link>
+            <Link to="/">Locations</Link>
+            <Link to="/">Language</Link>
+            <Link to="/">verified</Link>
+          </div>
+          <div className="copyright">
+            <p >© 2023 INSTAGRAM FROM META</p>
+          </div>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default Rightsidebar
+export default Rightsidebar;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -7,32 +7,7 @@ function Home() {
   return (
     <>
       <div className="main-home-container">
-        <div className="stories">
-          stories
-        </div>
-        <div className="posts">
-          <div className="header-posts">
-            <div className="left-header-post">
-              <img src={img1} alt="" />
-              <span className='username-header'>Username</span>
-              <span className="time">21m</span>
-            </div>
-            <div className="right-header-post">
-              <button>
-                <TbAntennaBars1 className='rotate90' />
-              </button>
-            </div>
-          </div>
-          <div className="image-post">
-            <img src={img1} alt="" />
-          </div>
-          <div className="likes-post">
-            
-            <LoveReactButton />
-            <FaCommentAlt />
-            <FaShareAlt/>
-          </div>
-        </div>
+        
       </div>
     </>
   )
